6acae57e2659154f2b7fd587d8d81b22578cd95d
eliot-akira <me@eliotakira.com>
2024-10-22 21:21:36 +0200

    Update version

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Tangible Updater module
  * Description: Updater for Tangible plugins and themes
- * Version: 2024.10.20
+ * Version: 2024.10.22
  * GitHub URI: TangibleInc/updater
  */
 require_once __DIR__ . '/index.php';
