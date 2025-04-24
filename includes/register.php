748a35c00f826f37080112e69333553334d463a0
eliot-akira <me@eliotakira.com>
2025-04-24 20:28:47 +0200

    Pass to function closure only variables being used

--- includes/register.php
+++ includes/register.php
@@ -61,7 +61,7 @@ function register_plugin($plugin) {
 
     $transient_key = 'tangible_updater_fail_update_status_' . $name;
 
-    add_filter('puc_request_info_result-' . $name, function($result, $url) use ($name, $transient_key) {
+    add_filter('puc_request_info_result-' . $name, function($result, $url) use ($transient_key) {
       if (isset($result->fail_update_status)) {
           set_transient($transient_key, $result->fail_update_status, 0);
       } else {
@@ -75,7 +75,7 @@ function register_plugin($plugin) {
     $basename = plugin_basename( $file );
     $action_name = "in_plugin_update_message-{$basename}";
 
-    add_action($action_name, function ($plugin) use ($name, $transient_key) {
+    add_action($action_name, function ($plugin) use ($transient_key) {
 
       $fail_update_status = get_transient( $transient_key );
       if (empty($fail_update_status)) return;
