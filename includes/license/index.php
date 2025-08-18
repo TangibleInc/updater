3dbac0f2b76faece20c573a346f3b63eee8a3c02
Ethelyn Matias <ethelyn.matias2015@gmail.com>
2025-08-18 19:27:52 +0800

    cron checker for expired plugin

--- includes/license/index.php
+++ includes/license/index.php
@@ -1,18 +1,23 @@
 <?php
+
 namespace tangible\updater;
+
 use tangible\framework;
 use tangible\updater;
 
 require_once __DIR__ . '/action.php';
 require_once __DIR__ . '/checker.php';
 require_once __DIR__ . '/page.php';
+require_once __DIR__ . '/Cron_Updater.php';
 
 // License key
-function get_license_key_setting_field() {
+function get_license_key_setting_field()
+{
   return updater::$instance->license_key_setting_field;
 }
 
-function get_license_key($plugin) {
+function get_license_key($plugin)
+{
   if (is_string($plugin)) {
     $plugin = framework\get_plugin($plugin);
     if (empty($plugin)) return;
@@ -22,10 +27,11 @@ function get_license_key($plugin) {
   $settings = framework\get_plugin_settings($plugin);
   $field = updater\get_license_key_setting_field();
 
-  return $settings[ $field ] ?? '';
+  return $settings[$field] ?? '';
 }
 
-function update_license_key($plugin, $license_key ='') {
+function update_license_key($plugin, $license_key = '')
+{
 
   if (is_string($plugin)) {
     $plugin = framework\get_plugin($plugin);
@@ -33,19 +39,21 @@ function update_license_key($plugin, $license_key ='') {
   }
 
   $field = updater\get_license_key_setting_field();
- 
+
   framework\update_plugin_settings($plugin, [
     $field => $license_key
   ]);
 }
 
 // Status key
-function get_license_status_setting_field() {
+function get_license_status_setting_field()
+{
   return updater::$instance->license_status_setting_field ?? 'license_status';
 }
 
-function get_license_status($plugin) {
-  
+function get_license_status($plugin)
+{
+
   if (is_string($plugin)) {
     $plugin = framework\get_plugin($plugin);
     if (empty($plugin)) return '';
@@ -57,17 +65,18 @@ function get_license_status($plugin) {
   return $settings[$field] ?? '';
 }
 
-function set_license_status($plugin, $status) {
+function set_license_status($plugin, $status)
+{
 
   if (is_string($plugin)) {
-      $plugin = framework\get_plugin($plugin);
-      if (empty($plugin)) return false;
+    $plugin = framework\get_plugin($plugin);
+    if (empty($plugin)) return false;
   }
 
   // Use the same helper function
-  $field = get_license_status_setting_field(); 
-  
+  $field = get_license_status_setting_field();
+
   return framework\update_plugin_settings($plugin, [
-      $field => $status
+    $field => $status
   ]);
 }
